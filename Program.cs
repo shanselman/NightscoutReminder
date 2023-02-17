5c97311a75486771a71ef18b38dd7eef7029496a
Sébastien Levert <slevert@microsoft.com>
2023-02-16 20:30:34 -0500

    Adding support for ToDo

--- Program.cs
+++ Program.cs
@@ -18,14 +18,35 @@ namespace NightscoutReminder
 
             foreach (var property in properties)
             {
-                var eventName = $"🚨 Your {property.Name?.ToLower()} is about to expire";
-                if (!await graphService.HasEvent(eventName, property.Expires))
-                {
-                    await graphService.AddEvent(eventName, property.Expires);
-                    Console.WriteLine($"Reminder for '{property.Name}' on {property.Expires} was added");
-                } else {
-                    Console.WriteLine($"Reminder for '{property.Name}' on {property.Expires} already exists");
-                }
+                var subject = $"🚨 Your {property.Name?.ToLower()} is about to expire";
+                await AddCalendarEvent(graphService, subject, property.Expires);
+                await AddTodoItem(graphService, subject, property.Expires);
+            }
+        }
+
+        private static async Task AddCalendarEvent(GraphService graphService, string subject, DateTime expires)
+        {
+            if (!await graphService.HasEvent(subject, expires))
+            {
+                await graphService.AddEvent(subject, expires);
+                Console.WriteLine($"Reminder '{subject}' on {expires} was added");
+            }
+            else
+            {
+                Console.WriteLine($"Reminder '{subject}' on {expires} already exists");
+            }
+        }
+
+        private static async Task AddTodoItem(GraphService graphService, string subject, DateTime expires)
+        {
+            if (!await graphService.HasTodoItem(subject, expires))
+            {
+                await graphService.AddTodoItem(subject, expires);
+                Console.WriteLine($"Task '{subject}' on {expires} was added");
+            }
+            else
+            {
+                Console.WriteLine($"Task '{subject}' on {expires} already exists");
             }
         }
     }
--- Services/GraphService.cs
+++ Services/GraphService.cs
@@ -4,112 +4,144 @@ using Microsoft.Graph.Models;
 
 namespace NightscoutReminder
 {
-  public class GraphService
-  {
-    private GraphServiceClient graphServiceClient;
-    private string? clientId;
-    private string[]? scopes;
-    public GraphService(string clientId, string scopes)
+    public class GraphService
     {
-      this.clientId = clientId;
-      this.scopes = scopes?.Split(',');
-
-      DeviceCodeCredentialOptions deviceCodeCredentialOptions = new DeviceCodeCredentialOptions()
-      {
-        ClientId = this.clientId
-      };
-
-      DeviceCodeCredential deviceCodeCredential = new DeviceCodeCredential(deviceCodeCredentialOptions);
-
-      //create client for calling v1 endpoint and get my info 
-      this.graphServiceClient = new GraphServiceClient(deviceCodeCredential, this.scopes);
-    }
-
-    public async Task AddEvent(string subject, DateTime expires)
-    {
-      // check if the reminder already exists
-      var events = await this.graphServiceClient.Me.Calendar.Events.GetAsync(r =>
-      {
-        r.QueryParameters.Filter = $"subject eq '{subject}'";
-      });
-
-      if (events?.Value?.Count > 0)
-      {
-        return;
-      }
-      var reminderEvent = new Event
-      {
-        Subject = subject,
-        Start = new DateTimeTimeZone
+        private GraphServiceClient graphServiceClient;
+        private string? clientId;
+        private string[]? scopes;
+        public GraphService(string clientId, string scopes)
         {
-          DateTime = expires.ToString("o"),
-          TimeZone = TimeZoneInfo.Local.Id
-        },
-        End = new DateTimeTimeZone
-        {
-          DateTime = expires.AddHours(1).ToString("o"),
-          TimeZone = TimeZoneInfo.Local.Id
+            this.clientId = clientId;
+            this.scopes = scopes?.Split(',');
+
+            DeviceCodeCredentialOptions deviceCodeCredentialOptions = new DeviceCodeCredentialOptions()
+            {
+                ClientId = this.clientId
+            };
+
+            DeviceCodeCredential deviceCodeCredential = new DeviceCodeCredential(deviceCodeCredentialOptions);
+
+            //create client for calling v1 endpoint and get my info 
+            this.graphServiceClient = new GraphServiceClient(deviceCodeCredential, this.scopes);
         }
-      };
 
-      // Add the event to the user's calendar
-      await this.graphServiceClient.Me.Calendar.Events.PostAsync(reminderEvent);
-    }
-
-    public async Task<bool> HasEvent(string subject, DateTime expires)
-    {
-      // check if the reminder already exists
-      var events = await this.graphServiceClient.Me.Calendar.Events.GetAsync(r =>
-      {
-        r.QueryParameters.Filter = $"subject eq '{subject}' and start/dateTime eq '{expires.ToString("o")}'";
-      });
-
-      return events?.Value?.Count > 0;
-    }
-
-    // add a todo item from the nightscout properties to microsoft graph
-    public async Task AddTodoItem(string subject, DateTime expires)
-    {
-      // check if the reminder already exists
-      var taskLists = await this.graphServiceClient.Me.Todo.Lists.GetAsync(r =>
-      {
-        r.QueryParameters.Filter = $"wellknownListName eq 'defaultList'";
-      });
-
-      if (taskLists?.Value?.Count > 0)
-      {
-        var taskList = taskLists.Value[0];
-
-        var reminderTask = new TodoTask
+        public async Task AddEvent(string subject, DateTime expires)
         {
-          Title = subject,
-          StartDateTime = new DateTimeTimeZone
-          {
-            DateTime = expires.ToString("o"),
-            TimeZone = TimeZoneInfo.Local.Id
-          },
-          DueDateTime = new DateTimeTimeZone
-          {
-            DateTime = expires.AddHours(1).ToString("o"),
-            TimeZone = TimeZoneInfo.Local.Id
-          }
-        };
+            // check if the reminder already exists
+            var events = await this.graphServiceClient.Me.Calendar.Events.GetAsync(r =>
+            {
+                r.QueryParameters.Filter = $"subject eq '{subject}'";
+            });
 
-        // Add the event to the user's calendar
-        await this.graphServiceClient.Me.Todo.Lists[taskList.Id].Tasks.PostAsync(reminderTask);
-      }
-    }
+            if (events?.Value?.Count > 0)
+            {
+                return;
+            }
+            var reminderEvent = new Event
+            {
+                Subject = subject,
+                Start = new DateTimeTimeZone
+                {
+                    DateTime = expires.ToString("o"),
+                    TimeZone = TimeZoneInfo.Local.Id
+                },
+                End = new DateTimeTimeZone
+                {
+                    DateTime = expires.AddHours(1).ToString("o"),
+                    TimeZone = TimeZoneInfo.Local.Id
+                }
+            };
 
-    // get user information from microsoft graph
-    public async Task<User?> GetMe()
-    {
-      return await this.graphServiceClient.Me.GetAsync();
-    }
+            // Add the event to the user's calendar
+            await this.graphServiceClient.Me.Calendar.Events.PostAsync(reminderEvent);
+        }
 
-    // get user's presence information from microsoft graph
-    public async Task<Presence?> GetPresence()
-    {
-      return await this.graphServiceClient.Me.Presence.GetAsync();
+        public async Task<bool> HasEvent(string subject, DateTime expires)
+        {
+            // check if the reminder already exists
+            var events = await this.graphServiceClient.Me.Calendar.Events.GetAsync(r =>
+            {
+                r.QueryParameters.Filter = $"subject eq '{subject}' and start/dateTime eq '{expires.ToString("o")}'";
+            });
+
+            return events?.Value?.Count > 0;
+        }
+
+        // add a todo item from the nightscout properties to microsoft graph
+        public async Task AddTodoItem(string subject, DateTime expires)
+        {
+            // check if the reminder already exists
+            var taskList = await this.GetDefaultTaskList();
+
+            if (taskList != null)
+            {
+                var reminderTask = new TodoTask
+                {
+                    Title = subject,
+                    DueDateTime = new DateTimeTimeZone
+                    {
+                        DateTime = expires.ToString("o"),
+                        TimeZone = TimeZoneInfo.Local.Id
+                    }
+                };
+
+                // Add the event to the user's calendar
+                await this.graphServiceClient.Me.Todo.Lists[taskList.Id].Tasks.PostAsync(reminderTask);
+            }
+        }
+
+        // Get default task list from graph api
+        public async Task<TodoTaskList?> GetDefaultTaskList()
+        {
+            try
+            {
+                var taskLists = await this.graphServiceClient.Me.Todo.Lists.GetAsync(r =>
+                {
+                    r.QueryParameters.Filter = $"displayName eq 'Tasks'";
+                });
+
+                if (taskLists?.Value?.Count > 0)
+                {
+                    return taskLists.Value[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        // check if a todo item already exists
+        public async Task<bool> HasTodoItem(string subject, DateTime expires)
+        {
+            // check if the reminder already exists
+            var taskList = await this.GetDefaultTaskList();
+
+            if (taskList != null)
+            {
+                var tasks = await this.graphServiceClient.Me.Todo.Lists[taskList.Id].Tasks.GetAsync(r =>
+                {
+                    r.QueryParameters.Filter = $"title eq '{subject}' and startDateTime/dateTime eq '{expires.ToString("o")}'";
+                });
+
+                return tasks?.Value?.Count > 0;
+            }
+
+            return false;
+        }
+
+        // get user information from microsoft graph
+        public async Task<User?> GetMe()
+        {
+            return await this.graphServiceClient.Me.GetAsync();
+        }
+
+        // get user's presence information from microsoft graph
+        public async Task<Presence?> GetPresence()
+        {
+            return await this.graphServiceClient.Me.Presence.GetAsync();
+        }
     }
-  }
 }
